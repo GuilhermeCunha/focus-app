618419cf61b822c765493e308ac0fcc57050a53a
Guilherme <guiscunha@gmail.com>
2021-12-13 18:39:35 -0300

    assets: adding logo

---
+++ app.config.js
@@ -0,0 +1,12 @@
+const appLogo = "./assets/logo.png";
+const appName = "Focus";
+
+export default {
+  splash: {
+    image: appLogo,
+  },
+  expo: {
+    name: appName,
+    icon: appLogo,
+  },
+};
